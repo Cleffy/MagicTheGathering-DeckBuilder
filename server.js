86047f66d92cf41ec5c6f7d15ca781a826a05290
dianabee <dianabee16@gmail.com>
2023-08-15 20:38:21 -0700

    added packages to server.js and started sequelize

--- server.js
+++ server.js
@@ -1,12 +1,37 @@
+const path = require('path');
 const express = require('express');
+const session = require('express-session');
+const expressHandlebars = require('express-handlebars');
 const routes = require('./controllers');
+const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
+const sequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const hbs = expressHandlebars.create({helpers});
+
+const sess = {
+    secret: 'this is the secret',
+    cookie: {
+        maxAge: 30*24*60*60*1000,
+        httpOnly: true,
+        secure: false, 
+        sameSite: 'strict',
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new sequelizeStore({
+        db: sequelize
+    })
+};
+app.use(session(sess));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
