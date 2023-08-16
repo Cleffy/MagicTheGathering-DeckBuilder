638f79d8eb5b210efbe68dafc0a4bdfdba87e379
Cleffy <falchard@live.com>
2023-08-15 20:57:28 -0700

    Added sequeilize hook to User.js

--- models/user.js
+++ models/user.js
@@ -6,31 +6,39 @@ class User extends Model {
     //TODO: Add password check
 }
 
-User.init( {
-    id: {
-        type: DataTypes.INTEGER,
-        allowNull: false, 
-        primaryKey: true,
-        autoIncrement: true
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false, 
+            primaryKey: true,
+            autoIncrement: true
+        },
+        userName: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        userEmail: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        userPassword: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
     },
-    userName: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    userEmail: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    userPassword: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-       
-        //TODO: add table info
-        //UserID: Must be unique
-        //UserName: Must be unique
-        //UserEmail: Must be unique
-        //UserPassword
+    {
+        hooks: {
+            beforeCreate: async (newUserData) => {
+            newUserData.password = await bcrypt.hash(newUserData.password, 10);
+            return newUserData;
+            },
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user',
     }
 );
 
