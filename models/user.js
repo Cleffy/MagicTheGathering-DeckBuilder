f828bf8b2d1ba981cb8edfdb418a062b76024815
larsnanderson.Lars <larsnanderson.wowlarz321@gmail.com>
2023-08-16 13:41:42 +1000

    user data table

--- models/user.js
+++ models/user.js
@@ -6,8 +6,26 @@ class User extends Model {
     //TODO: Add password check
 }
 
-User.init(
-    {
+User.init( {
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false, 
+        primaryKey: true,
+        autoIncrement: true
+    },
+    userName: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    userEmail: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    userPassword: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+       
         //TODO: add table info
         //UserID: Must be unique
         //UserName: Must be unique
