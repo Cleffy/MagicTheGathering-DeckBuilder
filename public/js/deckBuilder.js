bcb7bcd9bd3e09e174e8945dcf6210e4bdfe263d
Cleffy <falchard@live.com>
2023-08-21 16:12:03 -0700

    Added deck pile
    Added a way to add cards to a deck and a pile to represent it.

--- public/js/deckBuilder.js
+++ public/js/deckBuilder.js
@@ -4,19 +4,32 @@ let buildAreaEl = document.getElementById('buildArea');
 let collection = new Array();
 let index = 0;
 let iterations = 0;
+let deck = new Array();
+let deckIter = 0
+let deckIndex = 0
 
+//when the window loads fetch the collection and render the screen
 window.onload = async function(){
     collection = await getFormat('standard');
     renderCollection();
+    renderDeck();
 }
 
+//get all the cards in a format
 async function getFormat(format){
     const response = await fetch(`/api/cards/format/${format}`);
     const data = await response.json();
     return data;
 }
+
+//Staging area for the collection
 function renderCollection(){
     collectionEl.innerHTML = '';
+    let bookEl = renderBook();
+    renderCard(bookEl);
+}
+//book backdrop to maybe represent a binder
+function renderBook(){
     let bookEl = document.createElement('section');
     let pageBackEl = document.createElement('div');
     let pageForwardEl = document.createElement('div');
@@ -51,34 +64,65 @@ function renderCollection(){
     collectionEl.appendChild(pageBackEl);
     collectionEl.appendChild(bookEl);
     collectionEl.appendChild(pageForwardEl);
-    renderBook(bookEl);
+    return bookEl;
 }
-
-function renderBook(bookEl,){
-    if(collection[index+iterations]){
+//individual cards
+function renderCard(bookEl){
+    for(let i = 0; i < 18; i++){
         let articleEl = document.createElement('article');
-        articleEl.className = 'card';
-        articleEl.id = `${(index + iterations)}`;
         let h4El = document.createElement('h4');
-        h4El.textContent = collection[index+iterations].cardName;
         let imgEl = document.createElement('img');
-        imgEl.className = 'cardImg';
-        imgEl.src = collection[index+iterations].image;
 
         articleEl.appendChild(imgEl);
         articleEl.appendChild(h4El);
-
         bookEl.appendChild(articleEl);
 
-        iterations++;
-        if(iterations == 18){
-            iterations = 0;
-            return;
-        }
+        articleEl.className = 'card';
+        
+        h4El.textContent = collection[index+i].cardName;
+
+        imgEl.className = 'cardImg';
+        imgEl.src = collection[index+i].image;
+        imgEl.addEventListener('click', (event) => {
+            event.stopPropagation();
+            deck.push(collection[index+i]);
+            renderDeck();            
+        });
     }
-    else{
-        iterations = 0;
-        return;
+}
+
+//Staging area for the deck
+function renderDeck(){
+    buildAreaEl.innerHTML = '';
+    createHeap();
+}
+//creates a pile of cards to represent the deck
+function createHeap(){
+    let heapEl = document.createElement('section');
+    let centerX = document.documentElement.clientWidth / 5;
+    let centerY = document.documentElement.clientHeight / 4;
+
+    heapEl.className = 'heap';
+    buildAreaEl.appendChild(heapEl);
+
+
+    for(let i = 0; i < deck.length; i++){
+        let divEl = document.createElement('div');
+        divEl.className = 'deckProtector';
+        let imageEl = document.createElement('img');
+        imageEl.className = 'cardImg';
+        imageEl.src = deck[i].image;
+        divEl.style.cssText =`
+        z-index: ${i};
+        left: ${Math.floor((Math.random() - 0.5) * 200) + centerX}px;
+        top: ${Math.floor((Math.random() - 0.5) * 200) + centerY}px;
+        transform: rotate(${(Math.random() - 0.5) * 15}deg)`;
+        divEl.appendChild(imageEl);
+        heapEl.appendChild(divEl);
+        imageEl.addEventListener('click', (event) => {
+            event.stopPropagation();
+            deck.splice(i, 1);
+            renderDeck();
+        });
     }
-    setTimeout(renderBook(bookEl), 100);
 }
