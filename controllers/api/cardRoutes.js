580e9b8ea2a4ba323a76451c36873882007d3cae
Claytonlax <130805846+Claytonlax@users.noreply.github.com>
2023-08-20 19:56:26 -0700

    card route

--- controllers/api/cardRoutes.js
+++ controllers/api/cardRoutes.js
@@ -1,17 +1,25 @@
-const router = require('express').Router();
-const { Card } = require('../../models');
+const router = require("express").Router();
+const { Card } = require("../../models");
 
+router.get("/", async (req, res) => {
+  try {
+    console.log(request.body);
+    const cardData = await Card.findAll(request.body);
+    response.status(200).json(cardData);
+  } catch (error) {
+    response.status(400).json(error);
+  }
+});
 
 //Create new card
-router.post('/', async (request, response) => {
-    try{
-            console.log(request.body);
-            const cardData = await Card.create(request.body);
-            response.status(200).json(cardData);
-    }
-    catch(error){
-        response.status(400).json(error);
-    }
+router.post("/", async (request, response) => {
+  try {
+    console.log(request.body);
+    const cardData = await Card.create(request.body);
+    response.status(200).json(cardData);
+  } catch (error) {
+    response.status(400).json(error);
+  }
 });
 
 module.exports = router;
