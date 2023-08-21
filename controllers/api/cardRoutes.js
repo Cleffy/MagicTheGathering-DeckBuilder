385dbc95b05251576904dcc9fc04561e58cb6d1f
Claytonlax <130805846+Claytonlax@users.noreply.github.com>
2023-08-20 19:23:30 -0700

    controllers, models, seeds, views

--- controllers/api/cardRoutes.js
+++ controllers/api/cardRoutes.js
@@ -1,18 +1,7 @@
 const router = require('express').Router();
 const { Card } = require('../../models');
 
-//const mtg = require('mtgsdk')
-/*
-mtg.card.find('', (result, request))
-.then(result => {
-    console.log(result.card.name) 
-})
 
-mtg.set.find()
-.then(result => {
-    console.log(result.set.name) 
-})
-*/
 //Create new card
 router.post('/', async (request, response) => {
     try{
--- models/deck.js
+++ models/deck.js
@@ -1,34 +1,45 @@
-const { Model, DataTypes } = require('sequelize');
-const bcrypt = require('bcrypt');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const bcrypt = require("bcrypt");
+const sequelize = require("../config/connection");
 
-class Deck extends Model {
-}
+class Deck extends Model {}
 
 Deck.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        complete: {
-            type: DataTypes.BOOLEAN,
-            allowNull: false
-        },
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-        sequelize,
-        timestamps: false,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'deck'
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
     },
+    complete: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+    },
+    //     // cards: {
+    //         type: DataTypes.ARRAY(DataTypes.STRING),
+
+    //     },
+    //     // userID: {
+    //         type: DataTypes.INTEGER,
+    //         references: {
+    //             model: 'User',
+    //             key: 'id'
+    //         }
+    //     }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "deck",
+  }
 );
 
 module.exports = Deck;
